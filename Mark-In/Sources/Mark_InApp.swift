f6ccc53bcdce02f06d547536cbaae08bc8d31d1b
Jeong Dong <eejxdong99@gmail.com>
2025-04-08 13:57:34 +0900

    프로젝트 재설정

---
+++ Mark-In/Sources/Mark_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mark_InApp.swift
+//  Mark-In
+//
+//  Created by 이정동 on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Mark_InApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
