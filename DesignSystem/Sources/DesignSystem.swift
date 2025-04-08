0df4126a3362d91b892ba093da604968bc7faf31
Jeong Dong <eejxdong99@gmail.com>
2025-04-08 16:29:34 +0900

    💄 DesignSystem Resource 추가

--- DesignSystem/Sources/DesignSystem.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DesignSystem.swift
-//  DesignSystem
-//
-//  Created by 이정동 on 4/8/25.
-//
-
-import Foundation
-
---
+++ DesignSystem/Sources/Image.swift
@@ -0,0 +1,16 @@
+//
+//  Image.swift
+//  DesignSystem
+//
+//  Created by 이정동 on 4/8/25.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let sampleImage: Image = Image(.test)
+}
+
+public extension ImageResource {
+    static let sampleImage: ImageResource = .test
+}
