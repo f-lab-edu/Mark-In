629280a939e09752c965d2071d23f241121cbfcb
Jeong Dong <eejxdong99@gmail.com>
2025-04-11 16:26:07 +0900

    [#1] Firebase SDK 추가 및 설정

--- Mark-In/Sources/Mark_InApp.swift
+++ Mark-In/Sources/Mark_InApp.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 import DesignSystem
 
+import FirebaseCore
+
 @main
 struct Mark_InApp: App {
   
   init() {
     FontLoader.registerFont()
+    
+    Self.configureFirebase()
   }
   
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
+  }
+}
+
+extension Mark_InApp {
+  static func configureFirebase() {
+#if DEBUG
+    let resource = "GoogleService-Info-Dev"
+#else
+    let resource = "GoogleService-Info"
+#endif
+    
+    guard let filePath = Bundle.main.path(forResource: resource, ofType: "plist"),
+          let options = FirebaseOptions(contentsOfFile: filePath)
+    else {
+      print("Firebase: \(resource) not found or invalid.")
+      return
+    }
+    
+    FirebaseApp.configure(options: options)
   }
 }
