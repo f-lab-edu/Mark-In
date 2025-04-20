1f379f658a7ef10cda8b320b1492607550ddc90a
Jeong Dong <eejxdong99@gmail.com>
2025-04-20 23:47:45 +0900

    [#8] LinkDTO 정의

---
+++ Mark-In/Sources/Data/DTOs/LinkDTO.swift
@@ -0,0 +1,19 @@
+//
+//  LinkDTO.swift
+//  Mark-In
+//
+//  Created by 이정동 on 4/20/25.
+//
+
+import Foundation
+
+struct LinkDTO: Codable {
+  var id: String
+  var url: String
+  var title: String
+  var thumbnailUrl: String?
+  var faviconUrl: String?
+  var createdBy: Date
+  var lastAccessedAt: Date?
+  var folderId: String?
+}
