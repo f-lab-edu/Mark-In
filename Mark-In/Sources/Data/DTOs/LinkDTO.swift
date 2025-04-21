5c5b9f97c0816423e0fb69f175f18b8c14e61526
Jeong Dong <eejxdong99@gmail.com>
2025-04-21 13:15:02 +0900

    [#8] LinkDTO initialize 및 toEntity 메서드 추가

--- Mark-In/Sources/Data/DTOs/LinkDTO.swift
+++ Mark-In/Sources/Data/DTOs/LinkDTO.swift
@@ -16,4 +16,28 @@ struct LinkDTO: Codable {
   var createdBy: Date
   var lastAccessedAt: Date?
   var folderId: String?
+  
+  init(_ link: Link) {
+    self.id = link.id
+    self.url = link.url
+    self.title = link.title
+    self.thumbnailUrl = link.thumbnailUrl
+    self.faviconUrl = link.faviconUrl
+    self.createdBy = link.createdBy
+    self.lastAccessedAt = link.lastAccessedAt
+    self.folderId = link.folderId
+  }
+  
+  func toEntity() -> Link {
+    Link(
+      id: self.id,
+      url: self.url,
+      title: self.title,
+      thumbnailUrl: self.thumbnailUrl,
+      faviconUrl: self.faviconUrl,
+      createdBy: self.createdBy,
+      lastAccessedAt: self.lastAccessedAt,
+      folderId: self.folderId
+    )
+  }
 }
