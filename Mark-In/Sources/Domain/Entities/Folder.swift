ac007caf0b0fb1bedc5b8dded23c6ce9ea681acf
Jeong Dong <eejxdong99@gmail.com>
2025-04-21 16:48:36 +0900

    [#12] Folder Entity 및 DTO 정의

---
+++ Mark-In/Sources/Domain/Entities/Folder.swift
@@ -0,0 +1,14 @@
+//
+//  Folder.swift
+//  Mark-In
+//
+//  Created by 이정동 on 4/21/25.
+//
+
+import Foundation
+
+struct Folder {
+  var id: String = ""
+  var name: String
+  var createdBy: Date = .now
+}
