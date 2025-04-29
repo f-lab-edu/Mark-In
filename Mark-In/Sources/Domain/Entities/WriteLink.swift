e6d0bf75f8100af555ef58edd12cd30793529d65
Jeong Dong <eejxdong99@gmail.com>
2025-04-29 14:48:26 +0900

    [#16] WriteLink Entity Model 추가

---
+++ Mark-In/Sources/Domain/Entities/WriteLink.swift
@@ -0,0 +1,14 @@
+//
+//  WriteLink.swift
+//  Mark-In
+//
+//  Created by 이정동 on 4/29/25.
+//
+
+import Foundation
+
+struct WriteLink {
+  var url: String
+  var title: String?
+  var folderId: String?
+}
