5f5c5ed3d9f2bb6e5728d933b6b96995e22990e4
Jeong Dong <eejxdong99@gmail.com>
2025-05-05 16:11:53 +0900

    [#28] RootView 구현

---
+++ Mark-In/Sources/App/RootView.swift
@@ -0,0 +1,53 @@
+//
+//  RootView.swift
+//  Mark-In
+//
+//  Created by 이정동 on 5/5/25.
+//
+
+import SwiftUI
+
+import DesignSystem
+
+struct RootView: View {
+  
+  @State private var rootViewModel = RootViewModel()
+  
+  var body: some View {
+    ZStack {
+      if rootViewModel.state.isSplashVisible {
+        SplashView()
+      } else {
+        if rootViewModel.sharedState.isLoginned {
+          MainView()
+        } else {
+          LoginView()
+        }
+      }
+    }
+    .onAppear {
+      rootViewModel.send(.onAppear)
+    }
+  }
+}
+
+
+private struct SplashView: View {
+  
+  var body: some View {
+    ZStack {
+      LinearGradient(
+        colors: [.markPoint, .markWhite],
+        startPoint: .top,
+        endPoint: .bottom
+      )
+      .ignoresSafeArea()
+      
+      Text("Splash View")
+    }
+  }
+}
+
+#Preview {
+  RootView()
+}
