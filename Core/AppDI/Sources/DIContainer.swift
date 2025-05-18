eb7d6f984cc619e3d1d6ee79df4b14de2d727c9d
Jeong Dong <eejxdong99@gmail.com>
2025-05-18 15:31:50 +0900

    [#43] AppDI 모듈 구현

---
+++ Core/AppDI/Sources/DIContainer.swift
@@ -0,0 +1,40 @@
+//
+//  DIContainer.swift
+//  DIContainer
+//
+//  Created by 이정동 on 5/18/25.
+//
+
+import Foundation
+
+public final class DIContainer {
+  public static let shared = DIContainer()
+  private var dependencies: [String: Any] = [:]
+  
+  private init() {}
+  
+  public func register<T>(_ dependency: T) {
+    let key = String(describing: T.self)
+    dependencies[key] = dependency
+  }
+  
+  func resolve<T>() -> T {
+    let key = String(describing: T.self)
+    let dependency = dependencies[key]
+    
+    guard let dependency = dependency as? T else {
+      fatalError("\(key)는 register되지 않았어어요. resolve 부르기전에 register 해주세요")
+    }
+    
+    return dependency
+  }
+}
+
+@propertyWrapper
+public struct Dependency<T> {
+  public let wrappedValue: T
+  
+  public init() {
+    self.wrappedValue = DIContainer.shared.resolve()
+  }
+}
