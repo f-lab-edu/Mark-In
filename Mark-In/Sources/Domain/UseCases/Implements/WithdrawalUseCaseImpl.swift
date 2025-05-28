5866d389240bd3a6d0d230f4ad86773e960628c0
Jeong Dong <eejxdong99@gmail.com>
2025-05-28 14:49:37 +0900

    [#48] WithdrawalUseCaseImpl 리팩토링

--- Mark-In/Sources/Domain/UseCases/Implements/WithdrawalUseCaseImpl.swift
+++ Mark-In/Sources/Domain/UseCases/Implements/WithdrawalUseCaseImpl.swift
@@ -41,24 +41,34 @@ struct WithdrawalUseCaseImpl: WithdrawalUseCase {
     
     switch provider {
     case .apple:
-      let token: String? = try? keychainStore.load(forKey: .refreshToken)
-      guard let token else { return }
-      
-      let url = URL(string: "https://\(Config.value(forKey: .revokeTokenURL))/revokeToken?refresh_token=\(token)"
-        .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
-      
-      _ = try await URLSession.shared.data(from: url)
-      
-      try keychainStore.delete(forKey: .refreshToken)
+      try await revokeAppleAuthorization()
       
     case .google:
-      try? await GIDSignIn.sharedInstance.disconnect()
-      GIDSignIn.sharedInstance.signOut()
+      try await revokeGoogleAuthorization()
       
     @unknown default:
-      fatalError("")
+      fatalError("Do not implement revoke function for provider: \(provider)")
     }
     
     authUserManager.clear()
+  }
+}
+
+private extension WithdrawalUseCaseImpl {
+  func revokeAppleAuthorization() async throws {
+    let token: String? = try? keychainStore.load(forKey: .refreshToken)
+    guard let token else { return }
+    
+    let url = URL(string: "https://\(Config.value(forKey: .revokeTokenURL))/revokeToken?refresh_token=\(token)"
+      .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
+    
+    _ = try await URLSession.shared.data(from: url)
+    
+    try keychainStore.delete(forKey: .refreshToken)
+  }
+  
+  func revokeGoogleAuthorization() async throws {
+    try? await GIDSignIn.sharedInstance.disconnect()
+    GIDSignIn.sharedInstance.signOut()
   }
 }
