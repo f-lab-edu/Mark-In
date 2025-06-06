3f766e927033951c9cf919a0c400803ca73af7b8
Jeong Dong <eejxdong99@gmail.com>
2025-06-06 18:15:53 +0900

    [#55] DeleteFolderUseCaseTests 파일 추가

---
+++ Mark-In-Tests/UseCaseTests/DeleteFolderUseCaseTests.swift
@@ -0,0 +1,61 @@
+//
+//  DeleteFolderUseCaseTests.swift
+//  Mark-In-Tests
+//
+//  Created by 이정동 on 6/6/25.
+//
+
+import Testing
+
+@testable import Mark_In
+
+struct DeleteFolderUseCaseTests {
+
+  @Test
+  func test_includingChildren이_true면_링크들을_삭제한다() async throws {
+    // Given: 준비
+    
+    // When: 실행
+    
+    // Then: 검증
+    
+    // TearDown: 해제
+    
+  }
+
+  @Test
+  func test_includingChildren이_false면_링크들을_기본_폴더로_이동한다() async throws {
+    // Given: 준비
+    
+    // When: 실행
+    
+    // Then: 검증
+    
+    // TearDown: 해제
+    
+  }
+  
+  @Test
+  func test_folderID가_nil이면_폴더는_삭제되지_않는다() async throws {
+    // Given: 준비
+    
+    // When: 실행
+    
+    // Then: 검증
+    
+    // TearDown: 해제
+    
+  }
+  
+  @Test
+  func test_folderID가_존재하면_폴더를_삭제한다() async throws {
+    // Given: 준비
+    
+    // When: 실행
+    
+    // Then: 검증
+    
+    // TearDown: 해제
+    
+  }
+}
