7927f858c7c67c00b15b5e2f7a7bba917092a1db
Jeong Dong <eejxdong99@gmail.com>
2025-06-06 14:40:07 +0900

    [#52] 테스트 메서드 이름 변경

--- Mark-In-Tests/ValidationsTests/DTOFieldKeyMappingTests.swift
+++ Mark-In-Tests/ValidationsTests/DTOFieldKeyMappingTests.swift
@@ -11,14 +11,14 @@ import Testing
 struct DTOFieldKeyMappingTests {
   
   @Test
-  func testCodingKeys_shouldMatchFirestoreFieldKeys_inFolderDTO() async throws {
+  func test_FolderDTO의_CodingKey와_FirestoreFieldKey가_일치해야_한다() async throws {
     #expect(FolderDTO.CodingKeys.id.rawValue == FirestoreFieldKey.Folder.id)
     #expect(FolderDTO.CodingKeys.name.rawValue == FirestoreFieldKey.Folder.name)
     #expect(FolderDTO.CodingKeys.createdAt.rawValue == FirestoreFieldKey.Folder.createdAt)
   }
   
   @Test
-  func testCodingKeys_shouldMatchFirestoreFieldKeys_inWebLinkDTO() async throws {
+  func test_WebLinkDTO의_CodingKey와_FirestoreFieldKey가_일치해야_한다() async throws {
     #expect(WebLinkDTO.CodingKeys.id.rawValue == FirestoreFieldKey.Link.id)
     #expect(WebLinkDTO.CodingKeys.url.rawValue == FirestoreFieldKey.Link.url)
     #expect(WebLinkDTO.CodingKeys.title.rawValue == FirestoreFieldKey.Link.title)
