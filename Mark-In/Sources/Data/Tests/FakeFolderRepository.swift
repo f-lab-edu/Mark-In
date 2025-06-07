24d7104a80d33558b96a12f2a00001884eb4d898
Jeong Dong <eejxdong99@gmail.com>
2025-06-07 16:38:32 +0900

    [#55] FakeFolderRepository 구현

--- Mark-In/Sources/Data/Tests/FakeFolderRepository.swift
+++ Mark-In/Sources/Data/Tests/FakeFolderRepository.swift
@@ -7,20 +7,53 @@
 
 import Foundation
 
-struct FakeFolderRepository: FolderRepository {
+final class FakeFolderRepository: FolderRepository {
+  var data: [String: [Folder]] = [:]
+  
   func create(userID: String, folder: WriteFolder) async throws -> Folder {
-    <#code#>
+    let folder = Folder(
+      id: UUID().uuidString,
+      name: folder.name,
+      createdAt: Date()
+    )
+    
+    data[userID, default: []].append(folder)
+    return folder
   }
   
   func fetchAll(userID: String) async throws -> [Folder] {
-    <#code#>
+    return data[userID] ?? []
   }
   
   func delete(userID: String, folderID: String) async throws {
-    <#code#>
+    data[userID]?.removeAll { $0.id == folderID }
   }
   
   func deleteAll(userID: String) async throws {
-    <#code#>
+    data[userID] = []
+  }
+}
+
+extension FakeFolderRepository {
+  func withTestFolders(userID: String, count: Int) -> Self {
+    let dummyFolders = (0..<count).map { _ in Folder.makeTestObject() }
+    data[userID, default: []].append(contentsOf: dummyFolders)
+    return self
+  }
+  
+  func withTestFolder(userID: String, folderID: String?) -> Self {
+    let dummyFolder = Folder.makeTestObject(id: folderID)
+    data[userID, default: []].append(dummyFolder)
+    return self
+  }
+}
+
+private extension Folder {
+  static func makeTestObject(
+    id: String? = UUID().uuidString,
+    name: String = "test-folder",
+    createdAt: Date = Date()
+  ) -> Folder {
+    Folder(id: id, name: name, createdAt: createdAt)
   }
 }
